5ebd75a135ae0ba019064c591faafbb45c938b2a
SaraBenitoPinel <sarabenito27@gmail.com>
2020-03-26 16:52:52 +0100

    ACABADO CON TODO

--- PokedexC_sharp/VentanaPrincipal.cs
+++ PokedexC_sharp/VentanaPrincipal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,24 +14,25 @@ namespace PokedexC_sharp
 {
     public partial class VentanaPrincipal : Form
     {
-        Conexion miConexion = new Conexion();
-        DataTable misPokemons = new DataTable();
-        DataTable misPokemonsNombre = new DataTable();
+        Conexion miConexion = new Conexion(); //PARA QUE COJA LA CONEXION
+        DataTable misPokemons = new DataTable(); //TABLA POR ID
+        DataTable misPokemonsNombre = new DataTable(); //TABLA NUEVA POR NOMBRE
 
-        int idactual = 0; //el pokemon que estamos viendo
+        int idactual = 0; //EL POKEMON QUE ESTAMOS VIENDO
+        private MySqlTransaction conexion;
 
         public VentanaPrincipal()
         {
             InitializeComponent();
         }
         private Image convierteBlobAImagen (byte[] img)
-        { //convierte la imagen
+        { //CONVIERTE LA IMAGEN
             MemoryStream ms = new System.IO.MemoryStream(img);
             return (Image.FromStream(ms));
         }
         private void datosPokemon()
         {
-            misPokemons = miConexion.getPokemonPorId(idactual);
+            //TE COLOCA TODOS LOS DATOS DE LOS POKEMONS CADA UNO EN SU LABEL
             nombrePokemon.Text = misPokemons.Rows[0]["nombre"].ToString();
             altura.Text = misPokemons.Rows[0]["altura"].ToString();
             peso.Text = misPokemons.Rows[0]["peso"].ToString();
@@ -48,40 +50,66 @@ namespace PokedexC_sharp
             descripcion.Text = misPokemons.Rows[0]["descripcion"].ToString();
             imagenpokemon.Image = convierteBlobAImagen((byte[])misPokemons.Rows[0]["imagen"]);
         }
+
         private void izquierda_Click(object sender, EventArgs e)
         {
+            //CAMBIA EL POKEMON HACIA LA IZQUIERDA Y SI PASA DE 0 NO TE DEJA MAS
             idactual--;
             if (idactual <= 0) { idactual = 1; }
+            misPokemons = miConexion.getPokemonPorId(idactual);
             datosPokemon();
         }
 
         private void derecha_Click(object sender, EventArgs e)
         {
+            //CAMBIA EL POKEMON HACIA LA DERECHA Y SI PASA DE 151 NO TE DEJA MAS
             idactual++;
             if (idactual >= 151) { idactual = 151; }
+            misPokemons = miConexion.getPokemonPorId(idactual);
             datosPokemon();
         }
 
 
         private void entrenador_Click(object sender, EventArgs e)
         {
+            //VENTANA QUE SE ABRE CUANDO PINCHO EL BOTON Y EN ESTE CASO ME PONE UNA IMAGEN
             Entrenador entrenador = new Entrenador();
             entrenador.Show();
         }
 
-        private void botonbuscador_Click(object sender, EventArgs e)//Esto para buscar pokemon por nombre
+        private void botonbuscador_Click(object sender, EventArgs e)
+            //ESTE BOTON TE BUSCA EL POKEMON SEGUN SU NOMBRE
         {
-            String textoBuscador = buscador.Text;
-            if(misPokemonsNombre.Rows.Count == 0)
+            String textoBuscador = buscador.Text; //PARA QUE ME LEA LO QUE ESCRIBO
+            //LO QUE ME HACE BUSCAR POR NOMBRE ESTA DECLARADO EN CONEXION
+            misPokemonsNombre = miConexion.getPokemonPorNombre(textoBuscador);
+            if (misPokemonsNombre.Rows.Count == 0)
             {
-                buscador.Text = "VACIO";
+                //ME DICE SI HAY DATOS DE LO BUSCADO
+                buscador.Text = "NO EXISTE";
             }
             else
             {
-
+                //PARA QUE ME BUSQUE POR NOMBRE USO LO QUE DECLARE ARRIBA COMO NUEVA TABLA
+                nombrePokemon.Text = misPokemonsNombre.Rows[0]["nombre"].ToString();
+                altura.Text = misPokemonsNombre.Rows[0]["altura"].ToString();
+                peso.Text = misPokemonsNombre.Rows[0]["peso"].ToString();
+                especie.Text = misPokemonsNombre.Rows[0]["especie"].ToString();
+                habilidad.Text = misPokemonsNombre.Rows[0]["habilidad"].ToString();
+                habitat.Text = misPokemonsNombre.Rows[0]["habitat"].ToString();
+                pre.Text = misPokemonsNombre.Rows[0]["PreEvolucion"].ToString();
+                pos.Text = misPokemonsNombre.Rows[0]["PosEvolucion"].ToString();
+                tipo1.Text = misPokemonsNombre.Rows[0]["tipo1"].ToString();
+                tipo2.Text = misPokemonsNombre.Rows[0]["tipo2"].ToString();
+                mov1.Text = misPokemonsNombre.Rows[0]["movimiento1"].ToString();
+                mov2.Text = misPokemonsNombre.Rows[0]["movimiento2"].ToString();
+                mov3.Text = misPokemonsNombre.Rows[0]["movimiento3"].ToString();
+                mov4.Text = misPokemonsNombre.Rows[0]["movimiento4"].ToString();
+                descripcion.Text = misPokemonsNombre.Rows[0]["descripcion"].ToString();
+                imagenpokemon.Image = convierteBlobAImagen((byte[])misPokemonsNombre.Rows[0]["imagen"]);
             }
-
             
         }
+
     }
 }
